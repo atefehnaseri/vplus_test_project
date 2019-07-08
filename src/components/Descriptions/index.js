a491e44379385a2f0a0028ec7aec1582ef232a38
Atefeh Naseri <ati_nas@yahoo.com>
2019-07-08 09:15:00 +0430

    Add files via upload

--- src/components/Descriptions/index.js
+++ src/components/Descriptions/index.js
@@ -1,1 +1,37 @@
+import React from 'react';
 
+export default function Descriptions() {
+    return (
+        <div className="descriptions-container">
+            <div className="descriptions">
+                <p className="title">
+                    <span>Sabavision Plus Features</span>
+                </p>
+                <div className="description">
+                    <img src="../../src/images/icon1.d83639ea.png" alt="icon" className="icon"/>
+                    <span className="description-text">
+                            <span>Creating unlimited programs and advertises in a campaign</span>
+                        </span>
+                </div>
+                <div className="description">
+                    <img src="../../src/images/icon2.f10e7ff4.png" alt="icon" className="icon"/>
+                    <span className="description-text">
+                            <span>Controling the cost of impressions by advertiser</span>
+                        </span>
+                </div>
+                <div className="description">
+                    <img src="../../src/images/icon3.7fae1541.png" alt="icon" className="icon"/>
+                    <span className="description-text">
+                            <span>Different targeting for each program</span>
+                        </span>
+                </div>
+                <div className="description">
+                    <img src="../../src/images/icon4.6b0a4d06.png" alt="icon" className="icon"/>
+                    <span className="description-text">
+                            <span>Advance reporting</span>
+                        </span>
+                </div>
+            </div>
+        </div>
+    )
+}
